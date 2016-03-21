a0227ac114316a304eb8dd5fc88c0f9a627afb14
sreddy70 <sreddy70@csc.com>
2016-03-21 15:18:48 +0530

    Initial commit

---
+++ MVCTestProject/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MVCTestProject.Startup))]
+namespace MVCTestProject
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
